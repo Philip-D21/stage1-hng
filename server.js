85935317ade31373b06ff50dcab93d59bb19a1a4
Philip-D21 <philiplekan88@gmail>
2022-11-01 03:00:40 -0700

    new commit

--- server.js
+++ server.js
@@ -4,15 +4,25 @@ const url = require("url");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
- if (req.method == 'GET' && req.url === '/' ) 
-        res.setHeader("content-type","application/json")
-        res.write(`{
-            "slackUsername":"Philip Daudu","age": 22,"backend": true, 
-            "bio":" I'm passionate about building, growing and collaborating with teams"
-           }`)
+    res.setHeader("content-type","application/json")
+    if (req.method !== 'GET') {
+        res.end(`{"error": "error in routes"}`)
+    } else {
+        if (req.url === '/') {
+           res.end(`{"slackUsername":"Philip Daudu","age": 22,"backend": true ,"bio":" I'm passionate about building, growing and collaborating with teams"}`)
+        }
+    }
+    });
+
+//  if (req.method == 'GET' && req.url == '/' ) 
+//         res.setHeader("content-type","application/json")
+//         res.write(`{
+//             "slackUsername":"Philip Daudu","age": 22,"backend": true, 
+//             "bio":" I'm passionate about building, growing and collaborating with teams"
+//            }`)
     
-        res.end();
-})
+//         res.end();
+// })
 
 server.listen(port, () => {
     console.log(`Server listening on port ${port}`);
