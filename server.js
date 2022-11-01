921cd4a69f5a6499357f4b093e9b087d233bce24
Philip-D21 <philiplekan88@gmail>
2022-10-31 18:51:03 -0700

    updated to GET req

--- server.js
+++ server.js
@@ -1,34 +1,126 @@
-const http = require("http");
-const path = require("path");
-const url = require("url");
+const http = require('http');
 
-const port = 5000
+const port = process.env.PORT || 3000
 
-const server= http.createServer( (req,res) => {
-   res.writeHead(200);
-   res.write(`{
-    "slackUsername":"Philip Daudu","age": 22,"backend":"true", 
-    "bio":" I'm passionate about building, growing and collaborating with teams"
-   }`)
-   res.end();
-});
-//     const reqUrl = url.parser(req.url).pathname
-    
-//    if(req.method =="GET"){ if(reqUrl == "/"){
-//     res.writeHead(200);
-//     res.end(`{
-//         "slackUsername":"Philip Daudu","age": 22,"backend":"true", "bio":" I'm passionate about building, growing and collaborating with teams"
-//     }`)
-// } else if(req.method == "POST"){ 
-//     if(reqUrl =="/home"){
-//     res.write("This is home page")
-//     res.end();
-// }
-// }
-//    }
-//});
-
-
-server.listen(port,()=>{
-    console.log("Server is up and running");
+const server = http.createServer((req, res) => {
+    if (req.method == 'GET' && req.url === '/api/user' ) {
+        res.end(`{
+            "slackUsername":"Philip Daudu","age": 22,"backend":"true", 
+            "bio":" I'm passionate about building, growing and collaborating with teams"
+           }`)
+    } else {
+        if (req.url === '/hello') {
+            res.end(`<h1>Hello</h1>`)
+        }
+    }
+    res.end(`{"error": "${http.STATUS_CODES[404]}"}`)
 })
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const http = require("http");
+// const path = require("path");
+// const url = require("url");
+
+// const port = 5000
+
+// const server= http.createServer( (req,res) => {
+//    res.writeHead(200);
+//    res.write(`{
+//     "slackUsername":"Philip Daudu","age": 22,"backend":"true", 
+//     "bio":" I'm passionate about building, growing and collaborating with teams"
+//    }`)
+//    res.end();
+// });
+// //     const reqUrl = http
+    
+// //    if(req.method =="GET"){ if(reqUrl == "/"){
+// //     res.writeHead(200);
+// //     res.end(`{
+// //         "slackUsername":"Philip Daudu","age": 22,"backend":"true", "bio":" I'm passionate about building, growing and collaborating with teams"
+// //     }`)
+// // } else if(req.method == "POST"){ 
+// //     if(reqUrl =="/home"){
+// //     res.write("This is home page")
+// //     res.end();
+// // }
+// // }
+// //    }
+// //});
+
+
+// server.listen(port,()=>{
+//     console.log("Server is up and running");
+// })
