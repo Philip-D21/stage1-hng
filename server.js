be2b2f1995fd2f555d6efa4f1e39e5e8fbbf8dba
Philip-D21 <philiplekan88@gmail>
2022-11-01 02:43:44 -0700

    new commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const url = require("url");
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-   // if (req.method == 'GET' && req.url === '/' ) 
+ if (req.method == 'GET' && req.url === '/' ) 
         res.setHeader("content-type","application/json")
         res.write(`{
             "slackUsername":"Philip Daudu","age": 22,"backend": true, 
